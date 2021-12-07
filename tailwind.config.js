edf5fefed5c16e955d77d0b646c69603f1020c35
melon-husk <ujwal5555@gmail.com>
2021-12-08 02:33:49 +0530

    added custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,18 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'dark-purple':'#101122',
+        'light-purple':'#D0D5FF',
+        'light-gray':"#6C6C6C",
+        'dark-red': '#C01010',
+        'light-red': '#E5C9D3',
+        'select-green': '#9FFFAF',
+        'select-blue':'#757DC2',
+        'bar-orange': '#FFC01D'
+      }
+    },
   },
   variants: {
     extend: {},
