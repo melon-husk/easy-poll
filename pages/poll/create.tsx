5e629c7f2edcac7c6c5f6a321e70bbd3c249cdaa
melon-husk <ujwal5555@gmail.com>
2021-12-09 02:53:51 +0530

    added spinner animation to deploying

--- pages/poll/create.tsx
+++ pages/poll/create.tsx
@@ -63,6 +63,7 @@ function reducer(state: State, action: any) {
 const CreatePoll = () => {
   const [state, dispatch] = useReducer(reducer, initialState);
   const [optionList, setOptionList] = useState<JSX.Element[]>([]);
+  const [deploying, setDeploying] = useState(false);
   const router = useRouter();
   useEffect(() => {
     dispatch({ type: "SET_POLL_ID", payload: uuidv4() });
@@ -77,6 +78,7 @@ const CreatePoll = () => {
     ]);
   }
   function handleDeployPoll() {
+    setDeploying(true);
     axios
       .post("/api/poll", {
         id: state.pollId,
@@ -84,8 +86,10 @@ const CreatePoll = () => {
         options: state.pollOptions,
       })
       .then((res) => console.log(res))
+      .then(() => setDeploying(false))
       .then(() => router.push(`/poll/${state.pollId}`));
   }
+
   return (
     <div className="flex flex-col min-h-screen px-2 pb-3 mx-auto md:w-9/12">
       <h1 className="my-10 text-5xl font-normal text-center text-light-purple">
@@ -109,11 +113,38 @@ const CreatePoll = () => {
         >
           Add Options
         </button>
+
         <button
           onClick={handleDeployPoll}
-          className="px-3 py-1 text-2xl font-semibold text-black transition-all duration-200 bg-light-purple rounded-2xl hover:bg-opacity-75 active:bg-opacity-50"
+          className="inline-flex items-center px-3 py-1 text-2xl font-semibold text-black transition-all duration-200 cursor-not-allowed bg-light-purple rounded-2xl hover:bg-opacity-75 active:bg-opacity-50"
         >
-          Deploy
+          {deploying ? (
+            <>
+              <svg
+                className="w-5 h-5 mr-3 -ml-1 animate-spin"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                <circle
+                  className="opacity-25"
+                  cx="12"
+                  cy="12"
+                  r="10"
+                  stroke="currentColor"
+                  strokeWidth="4"
+                ></circle>
+                <path
+                  className="opacity-75"
+                  fill="currentColor"
+                  d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                ></path>
+              </svg>
+              Deploying...
+            </>
+          ) : (
+            "Deploy"
+          )}
         </button>
       </div>
     </div>
